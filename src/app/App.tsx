66bea0c8aec793590a214ba5e7d541f0a5e12d32
yash khude <khudeyash2021@gmail.com>
2026-04-06 22:28:54 +0530

    clean all files

--- src/app/App.tsx
+++
@@ -1,6 +0,0 @@
-import { RouterProvider } from 'react-router';
-import { getRouter } from './routes';
-
-export default function App() {
-  return <RouterProvider router={getRouter()} />;
-}
--- src/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-import LoginPage from '../pages/LoginPage';
-
-export default LoginPage;
